44859b922bec5d25719411d06784fa08932bae15
Kim Pettersen <kim.a.pettersen@gmail.com>
2012-07-24 16:06:15 -0700

    removed old script + minor changes

--- learner_app/learner_app/views.py
+++ learner_app/learner_app/views.py
@@ -2,7 +2,7 @@ from pyramid.view import view_config
 from pyramid.httpexceptions import HTTPFound
 from models import Images
 
-@view_config(route_name='home', renderer='templates/mytemplate.pt')#renderer='json')#renderer='templates/mytemplate.pt')
+@view_config(route_name='home', renderer='templates/mytemplate.pt')
 def show_img(request):
 	res = request.db.paths.find_one({ 'label' : '' })
 	return { 'image' : res }
@@ -20,9 +20,7 @@ def save_label(request):
 				request.db.paths.save(query)
 				return HTTPFound(location='/')
 			except:
-				#db unavailable
 				return { 'error' : 'Database not available' }
 		except:
-			#Post data not valid
 			return HTTPFound(location='/')			
 	return HTTPFound(location='/')
